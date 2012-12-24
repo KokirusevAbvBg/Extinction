8856ee703b513c340551ff3da28fcdb3c21948b0
NikIvRu <kokirusev@abv.bg>
2012-12-24 19:07:13 +0200

    register.php

---
+++ register.php
@@ -0,0 +1,26 @@
+<html>
+
+    <?php
+        require_once('connect_db.php');
+        require_once('functions.php'); 
+
+        if(isset($_POST['submit'])){
+            register();
+        }
+
+    include("header.php");
+    ?>
+    
+
+        <form name="regform" class="regform" method="post">
+            <fieldset>
+                <div><label for="username">Nickname:</label><input type='text' name='username' /><br /></div>
+                <div><label for="password">Password:</label><input type='password' name='password' /><br /> </div>
+                <div><label for="confirm">Confirm Password:</label><input type='password' name='confirm' /><br /></div>
+                <div><label for="confirm">Email:</label> <input type='test' name='email' /><br /> </div>
+                <input type='submit' value='Register!' name="submit" class="regbutton" />
+            </fieldset>
+        </form>
+    </body>
+
+</html>
